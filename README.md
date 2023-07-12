4522791f77e18c1ef61e6e53659956b8dbe52ee9
tuhao0719 <kai256938@163.com>
2023-07-12 11:39:07 +0800

    index

--- README.md
+++
@@ -1,3 +0,0 @@
-#这是一个仓库
-
- <img src="images/warehouse.png" alt="这是一个仓库">
--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -5,7 +5,8 @@
 let myImage = document.querySelector('img');
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.png') {
+    if(mySrc === 'images/firefox-icon.png')
+    {
       myImage.setAttribute('src', 'images/firefox2.png');
     } else {
       myImage.setAttribute('src', 'images/firefox-icon.png');
